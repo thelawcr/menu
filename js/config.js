87bfa38f71b699194d623c8b31b64d16969b654f
Jorge Zamora <38996971+thelawcr@users.noreply.github.com>
2026-08-15 01:57:55 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = 'https://wqpypmsjkqdlfxkwypvn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcHlwbXNqa3FkbGZ4a3d5cHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NjU1NDYsImV4cCI6MjEwMjM0MTU0Nn0.SI72uBl3FoJp9vIZVU_YvM2ppUN6BihHAhU3_ES2woY';
 
+if (!window.supabase) {
+  console.error('No se cargó Supabase JS. Revisa el script CDN en las páginas HTML.');
+}
+
+const supabaseClient = window.supabase?.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
