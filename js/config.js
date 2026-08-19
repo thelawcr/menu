2fb3bc4ad5d82c2b132bc97f0fec2b3f8b18db92
Jorge Zamora <38996971+thelawcr@users.noreply.github.com>
2026-08-19 12:25:02 -0600

    Delete js/config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = 'https://wqpypmsjkqdlfxkwypvn.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_3WBae2vZGfMLVsX414FYLg_Lo5cNDVv';
-
-if (!window.supabase) {
-  console.error('No se cargó Supabase JS. Revisa el script CDN en las páginas HTML.');
-}
-
-const supabaseClient = window.supabase?.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
